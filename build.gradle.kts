c528e22dc72b17e44b24659133fb0b77a7110a82
l4zs <business@l4zs.de>
2022-08-05 01:26:28 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,68 @@
+val javaVersion = 17
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+    `maven-publish`
+    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
+}
+
+group = "de.l4zs"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper", "paper-api", "1.19.1-R0.1-SNAPSHOT")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            freeCompilerArgs = listOf(
+                "-Xjdk-release=$javaVersion",
+            )
+            jvmTarget = "$javaVersion"
+        }
+    }
+    compileJava {
+        options.encoding = "UTF-8"
+        options.release.set(javaVersion)
+    }
+    publishToMavenLocal {
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            groupId = project.group.toString()
+            artifactId = project.name.toLowerCase()
+            version = project.version.toString()
+
+            pom {
+                name.set(project.name)
+                description.set(project.description)
+
+                developers {
+                    developer {
+                        name.set("l4zs")
+                    }
+                }
+
+                licenses {
+                    license {
+                        name.set("GNU General Public License, Version 3")
+                        url.set("https://www.gnu.org/licenses/gpl-3.0.en.html")
+                    }
+                }
+
+                url.set("https://github.com/l4zs/translations")
+            }
+        }
+    }
+}
